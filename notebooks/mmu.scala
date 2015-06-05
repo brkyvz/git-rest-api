b98beeeeed4d6fc73fa9e5e58f4f11559d947f0f
Burak Yavuz <brkyvz@gmail.com>
2015-06-04 20:55:34 -0700

    This one takes the cake

--- notebooks/mmu.scala
+++ notebooks/mmu.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Fri, 5 Jun 2015 03:32:58 UTC
+// Databricks notebook source exported at Fri, 5 Jun 2015 03:55:32 UTC
 hohohohohohohoh
 
 // COMMAND ----------
@@ -8,6 +8,10 @@ hohohohohohohoh
 // COMMAND ----------
 
 1 + 1
+
+// COMMAND ----------
+
+ 1 - 1
 
 // COMMAND ----------
 
