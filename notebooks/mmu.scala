c2f12bab60d79f32bf61574a57510b3c9265abf3
Burak Yavuz <brkyvz@gmail.com>
2015-06-04 20:32:59 -0700

    spark rulz

--- notebooks/mmu.scala
+++ notebooks/mmu.scala
@@ -1,9 +1,13 @@
-// Databricks notebook source exported at Fri, 5 Jun 2015 03:31:25 UTC
+// Databricks notebook source exported at Fri, 5 Jun 2015 03:32:58 UTC
 hohohohohohohoh
 
 // COMMAND ----------
 
 2+2
+
+// COMMAND ----------
+
+1 + 1
 
 // COMMAND ----------
 
