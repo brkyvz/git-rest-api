545bb0436f496cad1345a0942558a37eecf3d08b
Burak Yavuz <brkyvz@gmail.com>
2015-06-04 20:31:26 -0700

    2015-06-04 20:31:25

--- notebooks/mmu.scala
+++ notebooks/mmu.scala
@@ -1,2 +1,9 @@
-// Databricks notebook source exported at Fri, 5 Jun 2015 03:28:14 UTC
+// Databricks notebook source exported at Fri, 5 Jun 2015 03:31:25 UTC
 hohohohohohohoh
+
+// COMMAND ----------
+
+2+2
+
+// COMMAND ----------
+
