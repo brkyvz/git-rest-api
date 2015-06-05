7961ad91440c21558e18604464f674793f0fa741
Burak Yavuz <brkyvz@gmail.com>
2015-06-04 20:57:50 -0700

    2015-06-04 20:57:48

--- notebooks/mme.scala
+++ notebooks/mme.scala
@@ -1,9 +1,13 @@
-// Databricks notebook source exported at Fri, 5 Jun 2015 03:57:19 UTC
+// Databricks notebook source exported at Fri, 5 Jun 2015 03:57:49 UTC
 3 + 3
 
 // COMMAND ----------
 
 1 + 1
+
+// COMMAND ----------
+
+0 - 2
 
 // COMMAND ----------
 
