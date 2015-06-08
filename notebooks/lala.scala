d07ceca30e357b804d950da09d35a548ce1cd20e
Burak Yavuz <brkyvz@gmail.com>
2015-06-08 14:34:32 -0700

    dede

--- notebooks/lala.scala
+++ notebooks/lala.scala
@@ -1,6 +1,6 @@
-// Databricks notebook source exported at Mon, 8 Jun 2015 21:32:33 UTC
+// Databricks notebook source exported at Mon, 8 Jun 2015 21:34:31 UTC
 1 + 2
 
 // COMMAND ----------
 
-1 - 1
+1 - 1 + 1
