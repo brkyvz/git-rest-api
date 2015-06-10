ccf6b953dc7149a1f06e34062d62890c1a50b76e
Burak Yavuz <brkyvz@gmail.com>
2015-06-10 13:36:40 -0700

    06-10-2015 20:36:39

--- notebooks/lala.scala
+++ notebooks/lala.scala
@@ -1,5 +1,12 @@
-// Databricks notebook source exported at Wed, 10 Jun 2015 00:46:13 UTC
-1 + 2 + 85
+// Databricks notebook source exported at Wed, 10 Jun 2015 20:36:39 UTC
+I am the very model of a cartoon individual, 
+My animation's comical, unusual, and whimsical,
+I'm quite adept at funny gags, comedic theory I have read,
+From wicked puns and stupid jokes to anvils that drop on your head.
+
+// COMMAND ----------
+
+1 + 2 + 85ufy
 
 // COMMAND ----------
 
