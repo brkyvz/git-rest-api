a67591a6abed68e89b180f69e49a2335eeec5183
Burak Yavuz <brkyvz@gmail.com>
2015-07-06 16:48:05 -0700

    huhu

--- notebooks/awesome.scala
+++ notebooks/awesome.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Fri, 3 Jul 2015 23:13:48 UTC
+// Databricks notebook source exported at Mon, 6 Jul 2015 23:48:04 UTC
 1 + 1 + 1 
 
 // COMMAND ----------
@@ -7,4 +7,4 @@
 
 // COMMAND ----------
 
-ho - 1
+ho 
