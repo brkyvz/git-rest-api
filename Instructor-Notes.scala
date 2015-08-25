45431a4e1bd3681c22a40dd676459657ca3b8aa1
Burak Yavuz <brkyvz@gmail.com>
2015-08-25 14:52:04 -0700

    let's see

--- Instructor-Notes.scala
+++ Instructor-Notes.scala
@@ -1,5 +1,9 @@
-// Databricks notebook source exported at Mon, 27 Jul 2015 17:54:55 UTC
-1 + 1 - 1
+// Databricks notebook source exported at Tue, 25 Aug 2015 21:52:03 UTC
+// MAGIC %run "/Users/burak/empty_history"
+
+// COMMAND ----------
+
+5 - 7
 
 // COMMAND ----------
 
