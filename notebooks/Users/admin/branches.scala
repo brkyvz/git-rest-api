ac883b460d9e8711d8adb8ddadbef2129f7e9a85
Burak Yavuz <brkyvz@gmail.com>
2016-01-25 16:46:26 -0800

    Update branches.scala

--- notebooks/Users/admin/branches.scala
+++ notebooks/Users/admin/branches.scala
@@ -1,10 +1,11 @@
-// Databricks notebook source exported at Wed, 30 Dec 2015 16:58:46 UTC
+// Databricks notebook source exported at Tue, 26 Jan 2016 00:46:26 UTC
 1 + 1
 
 // COMMAND ----------
 
-2 + 9
+2 + 9 - 7 + abc
 
 // COMMAND ----------
 
-0.5
+0.50.5
+val a = "bcd"
